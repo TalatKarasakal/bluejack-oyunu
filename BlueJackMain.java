d61213f3df1cb87c520091c43af70aabb1473b85
TalatKarasakal <talatkarasakal35@gmail.com>
2023-12-15 23:31:56 +0300

    Introduction to gameplay
    Basic things for gameplay have been prepared.

--- BlueJackMain.java
+++ BlueJackMain.java
@@ -4,5 +4,7 @@ public class BlueJackMain {
     public static void main(String [] args)throws Exception{
         ShufflingCards shufflingCards = new ShufflingCards();
         System.out.println(shufflingCards);
+        PlayingAlgorithm PlayingAlgorithm = new PlayingAlgorithm();
+        System.out.println(PlayingAlgorithm);
     }
 }
